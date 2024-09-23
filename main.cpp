7e24186203efe8e230f462c37f3c626d0e6216a2
AlfonsoAV10 <24020052@uadeo.mx>
2024-09-23 00:17:28 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre, apellido;
+    cout << "Por favor, introduce tu nombre: ";
+    cin >> nombre;
+    cout << "Ahora introduce tu apellido: ";
+    cin >> apellido;
+    cout << "Hola, " << nombre << " " << apellido << endl;
+    return 0;
+}
